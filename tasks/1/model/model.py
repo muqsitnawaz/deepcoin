e31f323fe363fd01ee9507ee6acb37b64b106d1b
sklin93 <sklin@connect.ust.hk>
2018-02-28 19:37:29 -0800

    Add codes for weight averaging

--- tasks/1/model/model.py
+++ tasks/1/model/model.py
@@ -2,7 +2,7 @@ from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D
 from keras.layers import Activation, Dropout, Flatten, Dense
 
-def model(input_shape):
+def model(input_shape=(150,150,3)):
 	model = Sequential()
 	model.add(Conv2D(32, (3, 3), input_shape=input_shape))
 	model.add(Activation('relu'))

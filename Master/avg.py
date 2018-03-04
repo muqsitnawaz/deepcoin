4de148b74ae6add3f7ca53e9b020b5283a53b784
sklin93 <sklin@connect.ust.hk>
2018-03-04 12:55:11 -0800

    Added winner selection

--- Master/avg.py
+++ Master/avg.py
@@ -1,14 +1,15 @@
 import h5py
 import os
 import numpy as np
+from numpy import linalg as LA
 import gc
-import sys
-from model import model
+import random
 
 taskId = 3
 model_dir = '../tasks/'+str(taskId)+'/model/'
-
+import sys
 sys.path.append(model_dir)
+from model import model
 
 print('Loading model...')
 model_base = model()
@@ -44,5 +45,26 @@ for f in src_files:
 		counter = counter+1
 model.set_weights(avg)
 model.save('avg.h5')
+
+# check the nodeId of the closest weight
+winId = 0
+minDist = 1000000.0
+# For tie breaking, select the first encountered minDist one 
+# Need shuffle directory to give everyone a chance
+random.shuffle(src_files) 
+for f in src_files:
+	if f.endswith(".h5") or f.endswith('.hdf5'):
+		curId = f[3]
+		full_f = os.path.join(srcDir, f)
+		tmp_model = model_base
+		tmp_model.load_weights(full_f)
+		cur_weight = tmp_model.get_weights()
+		curDist = 0.0
+		for i in range(num_layers):
+			curDist = curDist+(LA.norm(cur_weight[i]-avg[i]))**2
+		if curDist < minDist:
+			minDist = curDist
+			winId = curId
+print(winId)
 
 exit()

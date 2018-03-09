d0e48628519e9c379366a009915bc902f89f9803
sklin93 <sklin@connect.ust.hk>
2018-03-08 22:30:44 -0800

    Modify path to fit AWS

--- master/max1.py
+++ master/max1.py
@@ -5,7 +5,7 @@ from operator import itemgetter
 
 taskId = 1
 nb_validation_samples = 2400
-model_dir = '../tasks/'+str(taskId)+'/model/'
+model_dir = '~/efs/tasks/'+str(taskId)+'/model/'
 import sys
 sys.path.append(model_dir)
 from model import model
@@ -15,30 +15,39 @@ print('Loading model...')
 model_base = model()
 print('Model Loaded.')
 print('Loading data...')
-validation_data_dir = '../tasks/'+str(taskId)+'/data/validation'
+validation_data_dir = '~/efs/tasks/'+str(taskId)+'/data/validation'
 test_datagen = ImageDataGenerator(rescale=1. / 255)
 validation_generator = test_datagen.flow_from_directory(validation_data_dir,
 				target_size=(150, 150),batch_size=20,class_mode='binary')
 print('Data loaded.')
 
-srcDir = 'weights/1/'
-src_files = os.listdir(srcDir)
+srcDir = '~/efs/data/'
+src_folders = os.listdir(srcDir)
+src_folders = [x for x in src_folders if os.path.isdir(x)] 
 
 file_acc = {}
 tmp_model = model_base
 tmp_model.compile(loss='binary_crossentropy',
 			optimizer='rmsprop',metrics=['accuracy'])
-for f in src_files:
-	if f.endswith(".h5") or f.endswith('.hdf5'):
-		full_f = os.path.join(srcDir, f)
+for i in range(len(src_folders)):
+	nodeId = src_folders[i]
+	full_f = srcDir+nodeId+'/tasks/'+str(taskId)+'/tmp.h5'
+	if os.path.isfile(full_f):
 		tmp_model.load_weights(full_f)
 		_, acc = tmp_model.evaluate_generator(validation_generator,
 						steps=nb_validation_samples // 20)
-		print('Accuracy of', f, acc)
-		file_acc[f]=acc
+		print('Accuracy of', nodeId, acc)
+		file_acc[nodeId]=acc
 
 file_acc = sorted(file_acc.items(), key=itemgetter(1), reverse=True)
 
-print(file_acc[0][0][3])
+print(file_acc[0][0])
+
+# save opt.h5 to all nodes
+winDir = srcDir+file_acc[0][0]+'/tasks/'+str(taskId)+'/tmp.h5'
+tmp_model.load_weights(winDir)
+for i in range(len(src_folders)):
+	save_dir = srcDir+src_folders[i]+'/tasks/'+str(taskId)+'/opt.h5'
+	tmp_model.save(save_dir)
 
 exit()
--- master/max2.py
+++ master/max2.py
@@ -5,7 +5,7 @@ from numpy import linalg as LA
 from operator import itemgetter
 
 taskId = 2
-model_dir = '../tasks/'+str(taskId)+'/model/'
+model_dir = '~/efs/tasks/'+str(taskId)+'/model/'
 import sys
 sys.path.append(model_dir)
 from model import model
@@ -18,28 +18,37 @@ print('Loading model...')
 model_base = model()
 print('Model Loaded.')
 print('Loading data...')
-(x_train, y_train), (x_test, y_test) = load_data(path1='../tasks/'+str(taskId)+'/data/train/train.npz',
-											path2='../tasks/'+str(taskId)+'/data/validation/test.npz',)
+(x_train, y_train), (x_test, y_test) = load_data(path1='~/efs/tasks/'+str(taskId)+'/data/train/train.npz',
+											path2='~/efs/tasks/'+str(taskId)+'/data/validation/test.npz',)
 x_test = sequence.pad_sequences(x_test, maxlen=100)
 print('Data loaded.')
 
-srcDir = 'weights/'+str(taskId)+'/'
-src_files = os.listdir(srcDir)
+srcDir = '~/efs/data/'
+src_folders = os.listdir(srcDir)
+src_folders = [x for x in src_folders if os.path.isdir(x)]
 
 file_acc = {}
 tmp_model = model_base
 tmp_model.compile(loss='binary_crossentropy',
 				optimizer='adam', metrics=['accuracy'])
-for f in src_files:
-	if f.endswith(".h5") or f.endswith('.hdf5'):
-		full_f = os.path.join(srcDir, f)
+for i in range(len(src_folders)):
+	nodeId = src_folders[i]
+	full_f = srcDir+nodeId+'/tasks/'+str(taskId)+'/tmp.h5'
+	if os.path.isfile(full_f):
 		tmp_model.load_weights(full_f)
 		_, acc = tmp_model.evaluate(x_test, y_test, batch_size=30)
-		print('Accuracy of', f, acc)
-		file_acc[f]=acc
+		print('Accuracy of', nodeId, acc)
+		file_acc[nodeId]=acc
 
 file_acc = sorted(file_acc.items(), key=itemgetter(1), reverse=True)
 
-print(file_acc[0][0][3])
+print(file_acc[0][0])
+
+# save opt.h5 to all nodes
+winDir = srcDir+file_acc[0][0]+'/tasks/'+str(taskId)+'/tmp.h5'
+tmp_model.load_weights(winDir)
+for i in range(len(src_folders)):
+	save_dir = srcDir+src_folders[i]+'/tasks/'+str(taskId)+'/opt.h5'
+	tmp_model.save(save_dir)
 
 exit()
